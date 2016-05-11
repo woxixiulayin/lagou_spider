cc84df4bcf6a3d0cbb475764cb264ccd1bd56e5f
Jackson Liu <416883813@qq.com>
2016-05-11 09:51:19 +0800

    refine spider object

--- spider.js
+++ spider.js
@@ -7,7 +7,8 @@ var headers = {
             'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/33.0.1750.154 Safari/537.36',
             'X-MicrosoftAjax': 'Delta=true'
         }; 
-//getHtml 是一个promise，使用promise.then来获得html
+
+//getHtml 是一个promise，使用异步操作在promise.then中来处理获得html的body
 var getHtml = function(url) {
     var options = {
         url: url,
@@ -27,21 +28,20 @@ var getHtml = function(url) {
     return promise;
 };
 
-function Spider (jds, workpromise) {
+function Spider () {
     this.urls = [];
-    this.workerpromises = [];
-    this.jds = jds;
-    this.workpromise = workpromise;
 };
 
-Spider.prototype.creatUrl = function (jd) {
-
-}
-
-Spider.prototype.end = function(callback){
-    return Promise.all(this.workerpromises).then(callback(results));
+Spider.prototype.setWorker = function (callback) {//从url获得body文本后调用this.worker进行后续操作
+    this.worker = callback;
 };
-
+Spider.prototype.addUrl = function (url){//每添加一个url，设置一个worker
+    this.urls.push(url);
+    var that = this;
+    getHtml(url).then((body) => {
+            that.worker(body);//处理body
+        });
+};
 
 var url1 = "http://www.lagou.com/jobs/positionAjax.json?city=%E4%B8%8A%E6%B5%B7&kd=%E5%89%8D%E7%AB%AF";
 
