172aa2ac4ff33a01a3dbb4143ea2bef469345715
Jackson Liu <416883813@qq.com>
2016-05-12 22:57:39 +0800

    use pit (asyc test) to test getHtml

--- __tests__/test.js
+++ __tests__/test.js
@@ -6,10 +6,11 @@ var spider = require('../spider');
 describe('check funciton in spider.js', () => {
     var url1 = "http://www.lagou.com/jobs/positionAjax.json?city=%E4%B8%8A%E6%B5%B7&kd=%E5%89%8D%E7%AB%AF";
     var getHtml = spider.getHtml;
-    getHtml(url1).then((info) => {
-            console.log(info);
+    pit('check getHtml', () => {
+        return getHtml(url1).then((body) => {
+            var result = JSON.parse(body);
+            expect(result).toBeDefined();
+            expect(result.success).toBe(true);
         });
-    // it('check getHtml', () => {
-    
-    // });
+    });
 });
