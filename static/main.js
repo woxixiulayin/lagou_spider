1592937f656d0187d206a391ca9e0b4ec913b619
Jackson Liu <416883813@qq.com>
2016-05-16 23:37:17 +0800

    add test_jobinfo, begin to finish display jobinfo

--- static/main.js
+++ static/main.js
@@ -13,7 +13,28 @@ function createjds (job) {
     })
 }
 
-var cities = ["全国","北京","上海","广州","深圳","武汉"];
+var display = {
+    var maxhight = 600;
+    var maxhobcount;
+    function createJobDiv(jobcount) {//job信息div圆柱
+        var div = document.createElement("DIV");
+        div.className = "job-count";
+        div.style.height = getJobDivHight(jobcount);
+
+    };
+
+};
+var cities = ["全国","北京","上海","广州","深圳","武汉"]; 
+ 
+ var TEST_JONDATA = {job: "前端",
+                data:[
+                {city:"全国",jobcount:},
+                {city:"北京",jobcount:3456},
+                {city:"上海",jobcount:3090},
+                {city:"广州",jobcount:2000},
+                {city:"深圳",jobcount:2500},
+                {city:"武汉",jobcount:1500},
+                ]};
 
 inputjob = $('inputjob');
 
