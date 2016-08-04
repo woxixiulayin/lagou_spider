9dbe8a4def2af5725b777313014bb5132ff80057
Jackson Liu <416883813@qq.com>
2016-08-04 18:19:58 +0800

    remove test

--- app.js
+++ app.js
@@ -1,20 +1,32 @@
-#!/usr/bin/env node
+'use strict'
+const  koa = require('koa');
+var  send = require('koa-send'),
+    serve = require('koa-static'),
+    body = require('koa-body-parser')(),
+    app = koa(),
+    router = require('koa-router')(),
+    Spider = require('./server/spider.js').Spider;
 
-var express = require("express");
-var app = express();
-var port = "8082";
+app.use(serve((__dirname + "/src")));
 
-app.use("/static", express.static("front"));
-
-app.get("/", function (req, res) {
-    console.log("new request");
-    res.sendFile(__dirname + "/front/html/index.html");
+router.get('/', function *() {
+        yield send(this, "./src/html/index.html");
 });
 
-app.post("/search", function (req, res) {
-    console.log("new post request");
-    console.log(req.params);
+
+router.post('/search', body, function *(next) {
+    let spider = new Spider(this.request.body);
+    yield spider.run().then((posts) => {
+        let data = [];
+        posts.forEach((ele, index) => {
+            data = data.concat(ele);
+        });
+        this.body = JSON.stringify(data);
+    });
 });
 
-app.listen(port);
-console.log("server running at port " + port);
+
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+app.listen(8082);
