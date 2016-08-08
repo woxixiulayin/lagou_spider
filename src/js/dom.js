d5fa2e2ef137e3a6d41b2ecc0205f21b38178277
Jackson Liu <416883813@qq.com>
2016-08-08 17:57:09 +0800

    add input event

--- src/js/dom.js
+++ src/js/dom.js
@@ -10,9 +10,7 @@ $("#submit").click(function() {
     showResult();
 });
 
-//先影藏cityBar，然后设置city sapn
-$cityBar.hide();
-search_cities.forEach(function(city, index) {
+function createCitySpan(city) {
     var $sapncity = $("<span></span>"),
         $removespan = $("<span></span>");
 
@@ -27,43 +25,65 @@ search_cities.forEach(function(city, index) {
             $removespan.stop().hide(400);
         });
 
+        if( search_cities.indexOf(city) === -1) {
+            search_cities.push(city);
+        }
     //removespan的click动作
     $removespan.addClass("remove_span")
         .click(function() {
+            var index = search_cities.indexOf(city);
             search_cities.splice(index, 1);
             //删除dom元素和对应的search_cities中的数据
-            $(this).parent().hide(400, function() {    
+            $(this).parent().hide(400, function() {
                 $(this).remove();
             })
         })
         .hide();
 
     $sapncity.append($removespan);
+    return $sapncity;
+}
+
+
+//先影藏cityBar，然后设置city sapn
+$cityBar.hide();
+search_cities.forEach(function(city, index) {
+    var $sapncity = createCitySpan(city);
     $cityBar.append($sapncity);
 });
 
+
+//添加城市按钮的事件
 var $addcity = $("<span></span>");
 $addcity.addClass("add_span");
 $cityBar.append($addcity);
-//cityBar 结束
+
+//创建input并获得焦点
+var $city_input = $("<input>");
+$city_input
+    .addClass("city_input")
+    .hide()
+    .blur(function() {
+        var city = this.value.toLowerCase();
+        if (city != "" && search_cities.indexOf(city) === -1 && /[\u4e00-\u9fa5].*/.test(city)) {
+            $sapncity = createCitySpan(city);
+            $addcity.before($sapncity)
+                .hide()
+                .show(400);
+        }
+        this.value = "";
+        $cityBar.find("input").hide(400);
+        return false;
+    });
+$addcity.after($city_input);
+
 
 //add_span点击事件
-$(".add_span").click(function () {
-    //创建input并获得焦点
-    var $city_input = $("<input>");
-    $city_input
-        .addClass("city_input")
-        .hide()
-        .blur(function() {
-
-        });
-    $(this).before($city_input);
+$(".add_span").click(function() {
     $city_input.show(400).focus();
-
-
 });
 
-//inputJob获得焦点是显示cityBar
+//inputJob获得焦点时候显示cityBar
 $inputJob.focus(function(event) {
     $cityBar.slideDown(500);
 });
